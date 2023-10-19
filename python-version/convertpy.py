dcf6e974f9cca35f4a5554be0bac15c479b268e4
syflairenicole <105584041+syflairenicole@users.noreply.github.com>
2023-10-19 15:04:56 -0400

    Update convertpy.py

--- python-version/convertpy.py
+++ python-version/convertpy.py
@@ -1,17 +1,18 @@
 from PIL import Image
 
-img = Image.open("rickastley2.png","r")
-img_pixels = list(img.getdata())
-
-numpixels = len(img_pixels)
-for i in range(numpixels):
-    img_pixels[i] = str(img_pixels[i])
-    rgba = img_pixels[i].split(", ")
-    for channel in range(len(rgba)):
-        if channel == 3:
-            img_pixels[i] = img_pixels[i][0:len(img_pixels[i]) - len(rgba[channel]) - 2]
-            img_pixels[i] = img_pixels[i] + ";"
-
-img_pixels_str = str(img_pixels)
-img_pixels_str = img_pixels_str.replace("'","").replace("(","").replace(")","").replace("[","").replace("]","").replace(";,",";").replace(" ","")
-img_pixels_str = img_pixels_str[0:len(img_pixels_str)-1].replace("\n","")
+def RetrievePixels(path):
+    img = Image.open(path,"r")
+    img_pixels = list(img.getdata())
+    
+    numpixels = len(img_pixels)
+    for i in range(numpixels):
+        img_pixels[i] = str(img_pixels[i])
+        rgba = img_pixels[i].split(", ")
+        for channel in range(len(rgba)):
+            if channel == 3:
+                img_pixels[i] = img_pixels[i][0:len(img_pixels[i]) - len(rgba[channel]) - 2]
+                img_pixels[i] = img_pixels[i] + ";"
+    
+    img_pixels_str = str(img_pixels)
+    img_pixels_str = img_pixels_str.replace("'","").replace("(","").replace(")","").replace("[","").replace("]","").replace(";,",";").replace(" ","")
+    img_pixels_str = img_pixels_str[0:len(img_pixels_str)-1].replace("\n","")
