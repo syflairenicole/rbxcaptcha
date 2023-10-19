9070523c4f7f5b0c0c8e3ffae5449802c2292a97
syflairenicole <105584041+syflairenicole@users.noreply.github.com>
2023-10-19 14:17:18 -0400

    Create convertjs.js

---
+++ javascript-version/convertjs.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const jimp = require("jimp");
+async function DecodePixels(path) {
+	try {
+		return jimp.read(path)
+		.then((image) => {
+			let bitmap = image.bitmap
+			var width = bitmap.width;
+			var height = bitmap.height;
+			var text = "";
+			for (var y = 0; y < height; y++) {
+			  for (var x = 0; x < width; x++) {
+				var pixel = jimp.intToRGBA(image.getPixelColor(x, y));
+				text = text + `${pixel.r}, ${pixel.g}, ${pixel.b}`
+				if (x < width - 1) {
+				    text = text + "; "
+				}
+			  }
+			}
+			text = text.replace(/\s/g,"").replace("\n","")
+			return text;
+		});
+	} catch(err) {
+		return "fail"
+		console.log(err)
+	}
+};
+
+
+async function RetrievePixels(path) {
+	const pixelData = await DecodePixels(path);
+	var pixelsString = pixelData.toString('utf8');
+	return pixelsString;
+};
+
+// RetrievePixels("image.png")
