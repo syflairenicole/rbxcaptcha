3e67d98a2efad9bc27269e48210c0852cc2039be
syflairenicole <105584041+syflairenicole@users.noreply.github.com>
2023-10-19 16:39:30 -0400

    Update server.js

--- javascript-version/server.js
+++ javascript-version/server.js
@@ -1,12 +1,29 @@
-const { convert } = require('convert-svg-to-png');
-const express = require('express');
+import {svg2png} from 'svg-png-converter'
+var svgCaptcha = require('svg-captcha');
+//const express = require('express');
 
-const app = express();
+//const app = express();
 
-app.post('/getcaptcha', async(req, res) => {
+async function generateCaptcha(options) {
+    var captchaObject = svgCaptcha.create(options || {
+      size: 6,
+      noise: 1,
+      color: false,
+    })
+  console.log(captchaObject.data)
+    const png = await svg2png({
+      input: captchaObject.data.toString().trim(),
+      encoding: 'dataURL', 
+      format: 'png',
+    });
+    console.log(png)
+}
+
+/*app.post('/getcaptcha', async(req, res) => {
   
   
   res.send(png);
 });
 
 app.listen(3000);
+*/
