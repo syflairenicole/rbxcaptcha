517cf0a001af975712698962b4f772116d5614ca
syflairenicole <105584041+syflairenicole@users.noreply.github.com>
2023-10-20 01:35:19 -0400

    Update server.js

--- javascript-version/server.js
+++ javascript-version/server.js
@@ -1,12 +1,27 @@
 const GenerateCaptcha = require("./captchagen.js");
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.static("public"));
+//app.use(express.static("public"));
+app.use((req, res, next) => {
+  let ipFilter = ["::12", "127.0.0.1"]; // basic security denying localhost access
+  let pass = true
+  console.log(req.connection.remoteAddress)
+  for (let i = 0; i < ipFilter.length; i ++) {
+    var badIp = ipFilter[i];
+    if (req.connection.remoteAddress.includes(badIp)) {
+      pass = false
+      res.send("{}")
+    }
+  };
+  if (pass == true) next();
+});
 
-app.get('/getcaptcha', async(req, res) => {
-  const size = req.param("size") || 8
-  res.json(GenerateCaptcha(parseInt(size)));
+app.get("/getcaptcha", async (req, res) => {
+  const size = req.param("size") || 8;
+  GenerateCaptcha(parseInt(size)).then((fres) => {
+    res.send(fres);
+  });
 });
 
 const listener = app.listen(process.env.PORT, () => {
