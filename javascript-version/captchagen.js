b5416cfe4e695b774e41d2257705038cf0cf2455
syflairenicole <105584041+syflairenicole@users.noreply.github.com>
2023-10-20 04:00:30 -0400

    Update captchagen.js

--- javascript-version/captchagen.js
+++ javascript-version/captchagen.js
@@ -9,12 +9,11 @@ var svgToPng = require('svg-png-converter').svg2png
 var svgCaptcha = require('svg-captcha');
 const RetrievePixels = require("./convertjs.js")
 
-async function generateCaptcha(options) {
-    options = options || {}
+async function generateCaptcha(size, noise) {
     var info = "fail"
-    var captchaObject = svgCaptcha.create(options || {
-      size: options.size | 8,
-      noise: options.noise | 1,
+    var captchaObject = svgCaptcha.create({
+      size: size | 8,
+      noise: noise | 1,
       color: false,
     })
     console.log(captchaObject.data)
